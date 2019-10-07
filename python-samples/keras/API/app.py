97f34cfce342b38d1cc6ee102a3daa578f360be7
Cesar Moura Leite Westphal <cesarmlwestphal@gmail.com>
2019-10-07 12:52:35 -0300

    First Flask implementation, two routes added

---
+++ python-samples/keras/API/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from prediction import predict
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return '''<h4 style="color: green">Welcome to the Garby prediction API, this is not intended to be seen by end users, but I am glad you are here <3</h4>'''
+
+@app.route('/predict', methods=['POST'])
+def predictResponse():
+    return predict()
---
+++ python-samples/keras/API/prediction.py
@@ -0,0 +1,2 @@
+def predict():
+    return [0]
